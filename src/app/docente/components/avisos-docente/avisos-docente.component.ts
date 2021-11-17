67963087976b71e8543e331c1e8db43c14d5f9ce
DaniMartz <48961782+DaniMartz@users.noreply.github.com>
2021-11-16 18:09:04 -0600

    aaaaa

--- src/app/docente/components/avisos-docente/avisos-docente.component.ts
+++ src/app/docente/components/avisos-docente/avisos-docente.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-avisos-docente',
@@ -6,10 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./avisos-docente.component.css']
 })
 export class AvisosDocenteComponent implements OnInit {
+  comments = [];
+  comForm: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.comForm = this.formBuilder.group({
+      aviso: ['']
+      
+    });
+
+    this.comments = [{
+      profe: 'Vicente Plata',
+      daysAgo: 5,
+      commentA: 'Comentario de apoyp para el grupo'
+    }]
+  }
+
+  AddComment(){
+    this.comments.push({profe: 'Vicente Plata', daysAgo: 1, commentA: this.comForm.value.aviso});
+    this.comForm = this.formBuilder.group({
+      aviso: ['']
+      
+    });
+    console.log(this.comments)
   }
 
 }
